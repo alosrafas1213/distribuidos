52748361a99fa9003b23a3dc4df80981f38a03d8
HectorMCT <hectorchavezt@outlook.com>
2020-03-24 10:56:48 -0600

    ALgoritmo de Berkeley Documentación

---
+++ Berkeley-master/Berkeley-master/esclavo.cpp
@@ -0,0 +1,202 @@
+//#include <cstdio>
+#include <cstdlib>
+#include <unistd.h>
+#include <cstring>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
+#include <iostream>
+#include <fstream>
+#include <sstream>
+//#include <ctime>
+#include <iomanip>
+#include <sys/time.h>
+using namespace std;
+
+struct timeval horaservidor;
+
+void error(const char *msg)
+{
+	perror(msg);
+	exit(0);
+}
+
+int main(int argc, char *argv[])
+{
+	/**
+		sockfd - Esta variable almacenan los valores devueltos por la llamada 
+	 			al sistema de socket y la llamada al sistema de aceptación.
+		numPuerto - Almacena el número de puerto en el que el servidor acepta conexiones. 
+		numCart - es el valor de retorno para las llamadas read () y write (); 
+				es decir, contiene el número de caracteres leídos o escritos.
+	 */
+	long sockfd, numPuerto, numCart;
+
+	/*
+		serv_addr - es una estructura que contiene una dirección de internet. 
+					Esta estructura se define en <netinet / in.h>. 
+					La variable serv_addr contendrá la dirección del servidor
+	*/
+	struct sockaddr_in serv_addr;
+	
+	/*
+		La variable server es un puntero a una estructura de tipo hostent. 
+		Esta estructura se define en el archivo de encabezado netdb.h, 
+		su definicion completa estara en el archvio de documentación.
+	*/
+	struct hostent *server;
+
+	// El reloj manejara nuestro cliente, para segundos y microsegundos 
+	double reloj = 0.0, reloj2=0.0;
+
+	// El servidor lee los caracteres de la conexión del socket en este búfer.
+	char buffer[256];
+
+	/*	El usuario debe pasar el número de puerto en el que el 
+		servidor aceptará conexiones como argumento. 
+	*/
+	if(argc < 2) {
+		fprintf(stderr,"uso puerto %s\n", argv[0]);
+		exit(0);
+	}
+	
+	/*	El número de puerto en el que el servidor escuchará las conexiones se 
+		pasa como argumento, y esta declaración utiliza la función atoi () para 
+		convertir esto de una cadena de dígitos a un entero.
+	*/
+	numPuerto = atoi(argv[1]);
+
+    //Obtenemos la hora del servidor
+	//Se hace la asignacion en segundos y microsegundos
+	gettimeofday(&horaservidor,NULL);				
+	reloj = horaservidor.tv_sec;
+	reloj2 = horaservidor.tv_usec;
+	reloj2 = reloj2 /1000000;
+	reloj = reloj + reloj2;
+			
+
+	/* 	La llamada al sistema socket () crea un nuevo socket. Se necesitan tres argumentos.
+		El primero es el dominio de dirección del socket. El dominio de Internet para cualquiera 
+		de los dos hosts en Internet (AF_INET). 
+		
+		El segundo argumento es el tipo de socket. Recuerde que hay dos opciones aquí, 
+		un socket de flujo en el que los caracteres se leen en un flujo continuo como si 
+		fueran de un archivo o tubería, y un socket de datagrama, en el que los mensajes 
+		se leen en fragmentos. Las dos constantes simbólicas son SOCK_STREAM y SOCK_DGRAM. 
+		
+		El tercer argumento es el protocolo. Si este argumento es cero (y siempre debería 
+		serlo excepto en circunstancias inusuales), el sistema operativo elegirá el protocolo 
+		más apropiado. Escogerá TCP para sockets de flujo y UDP para sockets de datagramas.
+	
+	*/
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	
+	/*	La llamada al sistema de socket devuelve una entrada en la tabla de descriptores de 
+		archivo (es decir, un entero pequeño). Si la llamada del socket falla, devuelve -1. 
+		En este caso, el programa muestra un mensaje de error y sale.
+	*/
+	if(sockfd < 0) 
+		error("ERROR abriendo socket");
+	
+
+	/*	La funcion gethostbyname() toma un nombre como argumento y devuelve un puntero a un 
+		servidor que contiene información sobre ese host. El campo contiene la dirección IP. 
+		Si esta estructura es NULL, el sistema no pudo localizar un host con este nombre.
+	*/
+	server = gethostbyname("localhost");
+	if(server == NULL) {
+		fprintf(stderr,"ERROR, no hay tal host\n");
+		exit(0);
+	}
+
+	/*	Este código establece los campos en serv_addr. Gran parte es lo mismo que en el servidor. 
+		La función bzero () establece todos los valores en un búfer a cero. Toma dos argumentos, 
+		el primero es un puntero al búfer y el segundo es el tamaño del búfer. Por lo tanto, esta 
+		línea inicializa serv_addr a ceros.
+		
+		Sin embargo, debido a que el servidor de server-> h_addr es una cadena de caracteres, usamos 
+		la función: bcopy().
+		Copia bytes de longitud de "(char *)server->h_addr" a "char *)&serv_addr.sin_addr.s_addr".
+		
+		La variable serv_addr es una estructura de tipo struct sockaddr_in. Esta estructura tiene 
+		cuatro campos. El primer campo es short sin_family, que contiene un código para la familia 
+		de direcciones. Siempre debe establecerse en la constante simbólica AF_INET.
+		
+		El segundo campo de serv_addr es sin_port, que contiene el número de puerto. Sin embargo, 
+		en lugar de simplemente copiar el número de puerto a este campo, es necesario convertirlo 
+		a orden de bytes de red utilizando la función htons () que convierte un número de puerto en 
+		orden de bytes de host a un número de puerto en orden de bytes de red.
+	
+	*/	
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+	serv_addr.sin_port = htons(numPuerto);
+	
+	/*	
+	
+	*/
+	if(connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+		error("ERROR conectando");
+
+    //Imprimiendo el reloj logico de la maquina local 
+	printf("Mi reloj  seg: %f microseg: %f\n\n",reloj,reloj2); 
+
+	//Se inicializa el buffer de intercambio de 256 bits de tamaño en cero.
+	bzero(buffer,256);
+    //Lectura del reloj logico del servidor de tiempo
+	numCart = read(sockfd,buffer,255);
+	if (numCart < 0) 
+		error("ERROR leyendo de socket");
+
+	//Imprimiendo el reloj logico del servidor de tiempo
+    cout << "Tiempo del servidor (TD) iniciaondo el algoritmo de Berkeley!\n"  << endl;
+
+    printf("Este es el reloj de TD: %f\n\n",atof(buffer));
+	
+	// se crean dos string's para la manipulaccion de los buffers
+	stringstream ss, ss1; // Se declaran ss y ssl como stringstream para poder manipularlas por medio del buffer
+	
+	ss << std::setprecision(20) << buffer; // Se inserta el contenido del buffer en ss
+	string tmpstr1 = ss.str(); // Se crea una string temporal para guardar lo almacenado en ss
+	
+	// Se convierte en relog de timepo del servidor de caracter a entero
+	double tmp = atof(tmpstr1.c_str());	 // Se convierte lo almacenado en la string temporal a entero y se almacena en tmp 
+	
+    // Cálculo de la diferencia de tiempo de la máquina local a partir del tiempo del server restando el valor de la variable reloj y la variable tmp
+    // Que contiene el rejoj del reloj logico del servidor
+	double diff = reloj - tmp; 		
+	cout << "Diferencia de tiempo respecto al reloj logico: "<< diff << endl;
+	
+	// Se limpia el buffer de mensaje
+	bzero(buffer,256);
+
+
+	//Se asigna el contenido de diff como string en ssl la variable strinstream previamente declarada
+	ss1 << std::setprecision(20) << diff;
+	// se asigna a una variable temporal
+	string tmpstr2 = ss1.str();
+	// Se copia su contenido al buffer
+	strcpy(buffer,tmpstr2.c_str());
+
+    // Enviar la diferencia horaria de esta máquina al servidor de tiempo
+	numCart = write(sockfd,buffer,strlen(buffer));	
+	if (numCart < 0) 
+		error("ERROR escribiendo al socket");
+
+	bzero(buffer,256);
+    // Lectura del valor promedio final a ajustar en el reloj lógico de la máquina local
+	numCart = read(sockfd,buffer,255);			
+	printf("Ajuste del reloj = %f\n",atof(buffer));
+
+	double adj_clock = atof(buffer);
+	
+	reloj = reloj + adj_clock;
+
+	printf("Mi reloj ajustado: %f\n",reloj);
+
+    //Cierre el socket del cliente y finalice la conexión.
+	close(sockfd);	
+	return 0;
+}
---
+++ Berkeley-master/Berkeley-master/maestro.cpp
@@ -0,0 +1,194 @@
+//#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <unistd.h>
+//#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+//#include <ctime>
+#include <sys/time.h>
+#include <iomanip>
+
+using namespace std;
+
+struct timeval horaservidor;
+
+void error(const char *msg) 
+{
+	perror(msg);
+	exit(1);
+}
+
+char buffer[256];
+int n;
+
+int total = 0, cnt = 0, t=0;
+double reloj = 0, reloj2 =0, promedio = 0; 
+
+void Berkeley(long newsockfd)
+{
+	bzero(buffer,256);
+
+	stringstream ss, ss1, ss2;
+	ss << std::setprecision(20) << reloj;                          //ss << std::setprecision(5) << num << endl;
+	// Se convierte el valor del reloj a String
+	string tmpstr1 = ss.str();			
+	// Ahora se convierte de String a const char *
+	strcpy(buffer,tmpstr1.c_str());				
+
+	// Se envia el tiempo del reloj logico conectado a la computadora
+	n = write((long)newsockfd,buffer,strlen(buffer));	
+	if (n < 0) error("ERROR escribiendo al socket");
+
+	bzero(buffer,256);
+	//Reading the specific time difference of connected machines
+	//Se lee la diferencia de tiempo entre las computadoras conectadas
+	read((long)newsockfd,buffer,255);			
+	cout << "Diferencia horaria de la máquina '" << newsockfd << "' : " << buffer << endl;
+	
+	ss1 << buffer;
+	string tmpstr2 = ss1.str();
+	
+	//converting Time Daemon's clock from char array to integer value
+	//Se convierte el valor del tiempo del reloj de char array a entero
+	double diff = atof(tmpstr2.c_str());	
+
+	//Adding all time differences
+	//Se suman las diferencias entre los tiempos
+	total = total + diff;			
+
+	sleep(2);
+
+	//Se obtiene el promedio de las diferencias en los tiempos
+	promedio = total/(cnt+1);			
+
+	//Se calcula el tiempo promedio de ajuste para cada reloj
+	double adj_clock = promedio - diff;		
+
+	bzero(buffer,256);
+	
+	ss2 << adj_clock;
+	string tmpstr3 = ss2.str();
+
+	//Se convierte el tiempo de ajuste a const char *
+	strcpy(buffer,tmpstr3.c_str());		
+
+	//Se envia el tiempo de ajuste a su respectiva maquina
+	n = write((long)newsockfd,buffer,strlen(buffer));	
+	if (n < 0) error("ERROR escribiendo al socket");
+}
+
+
+void *NewConnection(void *newsockfd) //La funcion de los hilos para cada una de las peticiones de los clientes
+{
+	if((long)newsockfd < 0) 
+		error("ERROR al aceptar");
+
+	
+	cout << "Conectado a la maquina: " << (long)newsockfd << endl;
+	cnt++;
+
+	while(cnt != t)
+	{
+		continue;
+	}
+
+	Berkeley((long)newsockfd);
+
+	close((long)newsockfd);
+	pthread_exit(NULL);
+}
+
+int main(int argc, char *argv[])
+{
+
+	long sockfd, newsockfd[10], numPuerto;
+	socklen_t clilen;
+	
+	struct sockaddr_in serv_addr, cli_addr;
+
+	//hilos para manejar solicitudes de clientes
+	pthread_t hilos[10];	
+
+	// El reloj manejara nuestro sservidor, para segundos y microsegundos 
+	gettimeofday(&horaservidor,NULL);				
+	reloj = horaservidor.tv_sec;
+	reloj2 = horaservidor.tv_usec;
+	reloj2 = reloj2 /1000000;
+	reloj = reloj + reloj2;
+
+	if (argc < 2) {
+		fprintf(stderr,"ERROR, no se ha asignado puerto\n");
+		exit(1);
+	}
+
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	
+	if(sockfd < 0) 
+		error("ERROR al abrir socket");
+
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	numPuerto = atoi(argv[1]);
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = INADDR_ANY;
+	serv_addr.sin_port = htons(numPuerto);
+	
+	int reuse = 1;
+
+    // Para reutilizar la dirección del socket en caso de bloqueos y fallas
+	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (const char*)&reuse, sizeof(reuse)) < 0)
+		perror("setsockopt(SO_REUSEADDR) ha fallado");
+
+	#ifdef SO_REUSEPORT
+	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEPORT, (const char*)&reuse, sizeof(reuse)) < 0) 
+		perror("setsockopt(SO_REUSEPORT) ha fallado");
+	#endif
+
+	if(bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+		error("ERROR durante binding");
+
+	listen(sockfd,10);
+	clilen = sizeof(cli_addr);
+
+	cout << "Ingrese el número total de máquinas para conectar: ";
+	cin >> t;
+
+	//cout << "Hora del reloj: " << reloj << endl;
+	printf("Hora del reloj: %f\n", reloj);
+
+	int sock_index = 0;
+	// Se crean las conexiones necesarias para las computadoras clientes
+	for(int i=0; i < t; i++)
+	{
+		newsockfd[sock_index] = accept(sockfd,(struct sockaddr *) &cli_addr,&clilen);
+
+		pthread_create(&hilos[i], NULL, NewConnection, (void *)newsockfd[sock_index]);
+		
+		sock_index=(sock_index+1)%100;
+	}
+	
+	for(int i=0; i<t; i++)
+	{
+		int rc = pthread_join(hilos[i], NULL);
+		if(rc)
+		{
+			printf("Error al unir hilo :\n");
+			cout << "Error: " << rc << endl;
+			exit(-1);
+		}
+	
+	}
+
+	cout << "Ajuste del reloj: " << promedio << endl;
+	reloj = reloj + promedio;
+
+	printf("Mi reloj ajustado: %f\n",reloj);
+
+	close(sockfd);
+	return 0; 
+	pthread_exit(NULL);
+}
